0ea5835659d864885fa09dfed509d6f1f202875a
Pramod Singh Jantwal <pramodsinghjantwal@gmail.com>
2026-06-07 18:47:47 +0530

    Add: longest-consecutive-sequence - submission-14

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-14.kt
@@ -0,0 +1,38 @@
+
+
+
+class Solution {
+    fun longestConsecutive(nums: IntArray): Int {
+        if (nums.size <= 1) return nums.size
+
+        val hash = HashSet<Int>()
+        for (i in nums) {
+            hash.add(i)
+        }
+
+        val sequenceStart = HashSet<Int>()
+        var curr = hash.first()
+        var i = 0
+        var processingSequence = false
+        for (i in hash) {
+            if(!hash.contains(i - 1)){
+                sequenceStart.add(i)                
+            }
+        }
+
+        print(sequenceStart.toString())
+
+        var longestSeqCount = 0        
+
+        for(start in sequenceStart) {
+            var currCount = 0
+            var currStart = start
+            while(hash.contains(currStart)){
+                currStart++
+                currCount++
+            }
+            longestSeqCount = maxOf(longestSeqCount,currCount)
+        }
+        return longestSeqCount
+    }
+}
