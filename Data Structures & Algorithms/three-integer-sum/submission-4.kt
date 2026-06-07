50ee491ad802c5f5175e8673b0b59c68407aa52e
Pramod Singh Jantwal <pramodsinghjantwal@gmail.com>
2026-06-08 00:57:38 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        nums.sort()
+        val list = hashSetOf<List<Int>>()
+        for(i in nums.indices){
+            var j = i + 1
+            var k = nums.size - 1
+            while(j < k) {
+                val sum = nums[i] + nums[j] + nums[k]
+
+                if(sum == 0){
+                    list.add(listOf(nums[i],nums[j],nums[k]))
+                }
+
+                if(sum > 0){
+                    k--
+                } else {
+                    j++
+                }
+            }
+        }
+        return list.toList()
+    }
+}
+
