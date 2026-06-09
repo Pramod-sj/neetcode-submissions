35a07f33aa3f7f91af3045a0f00c446c2bded740
Pramod Singh Jantwal <pramodsinghjantwal@gmail.com>
2026-06-09 22:13:51 +0530

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.kt
@@ -0,0 +1,27 @@
+
+class Solution {
+
+    val stack = ArrayDeque<String>()
+
+    fun evalRPN(tokens: Array<String>): Int {
+        for (token in tokens) {
+            if (token == "+") {
+                stack.addLast((stack.removeLast().toInt() + stack.removeLast().toInt()).toString())
+            } else if (token == "-") {
+                val last = stack.removeLast().toInt()
+                val secondLast = stack.removeLast().toInt()
+                stack.addLast((secondLast - last).toString())
+            } else if (token == "*") {
+                stack.addLast((stack.removeLast().toInt() * stack.removeLast().toInt()).toString())
+            } else if (token == "/") {
+                val last = stack.removeLast().toInt()
+                val secondLast = stack.removeLast().toInt()
+                stack.addLast((secondLast / last).toString())
+            } else {
+                stack.addLast(token)
+            }
+            print(" $stack ")
+        }
+        return stack.last().toInt()
+    }
+}
