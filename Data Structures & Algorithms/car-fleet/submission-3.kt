c0803670802116b6c83fc76300f7031cb3fe75ee
Pramod Singh Jantwal <pramodsinghjantwal@gmail.com>
2026-06-12 00:28:55 +0530

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.kt
@@ -0,0 +1,23 @@
+
+
+class Solution {
+    fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
+
+        val zip = position.zip(speed).sortedByDescending { it.first }
+        val newPosition = zip.map { it.first }
+        val newSpeed = zip.map { it.second }
+
+        val stack = ArrayDeque<Double>()
+        var fleetCount = 0
+
+        for (i in newPosition.indices) {
+            val time = (target - newPosition[i]) / newSpeed[i].toDouble()
+            if(stack.isNotEmpty() &&  time <= stack.last() ){
+
+            } else {
+                stack.addLast(time)
+            }
+        }
+        return stack.size
+    }
+}
